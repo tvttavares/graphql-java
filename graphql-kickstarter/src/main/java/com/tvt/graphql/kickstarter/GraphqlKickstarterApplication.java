1676e951aed20f0fc36bf071c05a55b3ea12178c
Thiago Tavares <tvttavares@gmail.com>
2021-08-27 18:05:58 -0400

    Initial module GraphQL Kickstarter

---
+++ graphql-kickstarter/src/main/java/com/tvt/graphql/kickstarter/GraphqlKickstarterApplication.java
@@ -0,0 +1,13 @@
+package com.tvt.graphql.kickstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlKickstarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlKickstarterApplication.class, args);
+    }
+
+}
---
+++ graphql-kickstarter/src/test/java/com/tvt/graphql/kickstarter/GraphqlKickstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvt.graphql.kickstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlKickstarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
