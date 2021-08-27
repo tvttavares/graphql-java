3b4626350a4c7796c7ec32d712ed148546e90ee5
Thiago Tavares <tvttavares@gmail.com>
2021-08-27 11:08:49 -0400

    Changing key for mismatch and how to solve it

--- book-details/src/main/java/com/bookdetails/GraphQLDataFetchers.java
+++ book-details/src/main/java/com/bookdetails/GraphQLDataFetchers.java
@@ -14,15 +14,18 @@ public class GraphQLDataFetchers {
     private static List<Map<String, String>> books = Arrays.asList(
             ImmutableMap.of("id", "book-1",
                     "name", "Harry Potter and the Philosopher's Stone",
-                    "pageCount", "223",
+//                    "pageCount", "223",
+                    "totalPages", "223",
                     "authorId", "author-1"),
             ImmutableMap.of("id", "book-2",
                     "name", "Moby Dick",
-                    "pageCount", "635",
+//                    "pageCount", "635",
+                    "totalPages", "635",
                     "authorId", "author-2"),
             ImmutableMap.of("id", "book-3",
                     "name", "Interview with the vampire",
-                    "pageCount", "371",
+//                    "pageCount", "371",
+                    "totalPages", "371",
                     "authorId", "author-3")
     );
 
