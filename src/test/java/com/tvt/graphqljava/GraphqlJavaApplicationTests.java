d36757957a0a37c1d5badd14b3eb5913d1e7d8f7
Thiago Tavares <tvttavares@gmail.com>
2021-08-26 20:19:49 -0400

    Initial commit

---
+++ src/test/java/com/tvt/graphqljava/GraphqlJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvt.graphqljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
