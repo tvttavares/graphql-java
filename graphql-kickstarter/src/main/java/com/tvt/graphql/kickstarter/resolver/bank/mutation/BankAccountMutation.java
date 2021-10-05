bfcda47b5767f490c28c428d748d541d3f4f08ca
Thiago Tavares <tvttavares@gmail.com>
2021-10-05 11:28:05 -0400

    Data type and input validation

--- graphql-kickstarter/src/main/java/com/tvt/graphql/kickstarter/resolver/bank/mutation/BankAccountMutation.java
+++ graphql-kickstarter/src/main/java/com/tvt/graphql/kickstarter/resolver/bank/mutation/BankAccountMutation.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Slf4j
@@ -18,15 +21,15 @@ import java.util.UUID;
 @Validated
 public class BankAccountMutation implements GraphQLMutationResolver {
 
-//    private final Clock clock;
+    private final Clock clock;
 
     public BankAccount createBankAccount(@Valid CreateBankAccountInput input) {
         log.info("Creating bank account for {}", input);
         return BankAccount.builder()
                 .id(UUID.randomUUID())
                 .currency(Currency.USD)
-//                .createdAt(ZonedDateTime.now(clock))
-//                .createdOn(LocalDate.now(clock))
+                .createdAt(ZonedDateTime.now(clock))
+                .createdOn(LocalDate.now(clock))
                 .build();
     }
 
