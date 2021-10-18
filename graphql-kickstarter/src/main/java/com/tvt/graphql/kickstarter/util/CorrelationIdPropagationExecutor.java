f92522a3598e6b1c9759e23809f8196a59a78525
Thiago Tavares <tvttavares@gmail.com>
2021-10-17 20:23:23 -0400

    Custom context

---
+++ graphql-kickstarter/src/main/java/com/tvt/graphql/kickstarter/util/CorrelationIdPropagationExecutor.java
@@ -0,0 +1,27 @@
+package com.tvt.graphql.kickstarter.util;
+
+
+import java.util.concurrent.Executor;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.MDC;
+
+@RequiredArgsConstructor(staticName = "wrap")
+public class CorrelationIdPropagationExecutor implements Executor {
+
+    private final Executor delegate;
+
+    @Override
+    public void execute(@NotNull Runnable command) {
+        var correlationId = MDC.get(CORRELATION_ID);
+        delegate.execute(() -> {
+            try {
+                MDC.put(CORRELATION_ID, correlationId);
+                command.run();
+            } finally {
+                MDC.remove(CORRELATION_ID);
+            }
+        });
+    }
+
+}
