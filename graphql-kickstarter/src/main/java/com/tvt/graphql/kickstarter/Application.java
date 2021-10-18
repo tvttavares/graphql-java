ee1e89053747ed9381f1dff37a39ff81712fcfde
Thiago Tavares <tvttavares@gmail.com>
2021-10-17 21:14:09 -0400

    Integration Testing (GraphQLTestTemplate)

--- graphql-kickstarter/src/main/java/com/tvt/graphql/kickstarter/Application.java
+++ graphql-kickstarter/src/main/java/com/tvt/graphql/kickstarter/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlKickstarterApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(GraphqlKickstarterApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
--- graphql-kickstarter/src/test/java/com/tvt/graphql/kickstarter/ApplicationTests.java
+++ graphql-kickstarter/src/test/java/com/tvt/graphql/kickstarter/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GraphqlKickstarterApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
